812a90aee7a7b7e435a6a7159150e940af7d1725
Phuong Tran Doan <phuongit1803@gmail.com>
2023-03-22 14:52:21 +0700

    Mobile menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
 import "./Header.scss";
 import avatar from "../assets/images/avatar.jpg";
-import { BiSearch } from "react-icons/bi";
+import { BiSearch, BiMenu } from "react-icons/bi";
+import { IoClose } from "react-icons/io5";
 import Search from "./Search";
 
 const Header = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [showSearch, setShowSearch] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
 
     const handleSearchClick = () => setShowSearch(!showSearch);
+
+    const handleShowMenu = () => setShowMenu(!showMenu);
 
     useEffect(() => {
         // Get data here
@@ -16,7 +20,7 @@ const Header = () => {
 
     return (
         <>
-            <div className="Header content">
+            <div className="Header desktop content">
                 <a className="brand-link" href="/">
                     Vân kể chuyện
                 </a>
@@ -60,6 +64,50 @@ const Header = () => {
                             <BiSearch className="icon" />
                             <span>Search</span>
                         </button>
+                    </div>
+                )}
+            </div>
+            <div className="Header mobile content">
+                <a className="brand-link" href="/">
+                    Vân kể chuyện
+                </a>
+                <div className="search-and-menu">
+                    <button
+                        className="search-button tetiary-button"
+                        onClick={handleSearchClick}
+                    >
+                        <BiSearch className="icon" />
+                    </button>
+                    <button
+                        className="toggle primary-button"
+                        onClick={handleShowMenu}
+                    >
+                        <BiMenu className="icon" />
+                    </button>
+                </div>
+                {showMenu && (
+                    <div className="collapse">
+                        <button
+                            className="toggle primary-button"
+                            onClick={handleShowMenu}
+                        >
+                            <IoClose className="icon" />
+                        </button>
+                        <a href="/">Home</a>
+                        <a href="/">About Vân</a>
+                        <a href="/">Contact</a>
+                        {isLoggedIn ? (
+                            <>
+                                <a href="/">Manage account</a>
+                                <a href="/">My favorites</a>
+                                <a href="/">Log out</a>
+                            </>
+                        ) : (
+                            <>
+                                <a href="/">Login</a>
+                                <a href="/">Sign up</a>
+                            </>
+                        )}
                     </div>
                 )}
             </div>
